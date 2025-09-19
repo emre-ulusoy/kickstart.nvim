114dcbeaad50520afc9e0e56b1cefee663c3eac0
Emre Ulusoy <emre5tornaci@gmail.com>
2025-09-19 22:03:19 +0300

    section for custom commands and ClearRegs

--- init.lua
+++ init.lua
@@ -218,6 +218,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- [[ Custom commands ]]
+vim.api.nvim_create_user_command("ClearRegs", [[let regs=split('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/-"', '\zs')|for r in regs|call setreg(r, [])|endfor]], {})
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
