5257fbc71c3f1b1fc47f0cec5b7e0cbc2fee298a
Emre Ulusoy <emre5tornaci@gmail.com>
2025-09-22 02:14:48 +0300

    treesitter context and its lazy-lock

--- init.lua
+++ init.lua
@@ -277,6 +277,8 @@ rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  'nvim-treesitter/nvim-treesitter-context',
+  -- 'nvim-treesitter/nvim-treesitter-textobjects', -- TODO: might wanna use this later on
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
