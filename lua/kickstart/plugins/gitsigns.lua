f07ec2b36f9da5a1213c1511b3d09614eaf1e961
Emre Ulusoy <emre5tornaci@gmail.com>
2025-09-21 13:54:21 +0300

    toggle git deleted

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -54,6 +54,7 @@ return {
         end, { desc = 'git [D]iff against last commit' })
         -- Toggles
         map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
+        map('n', '<leader>td', gitsigns.toggle_deleted, { desc = '[T]oggle git [D]eleted' })
         map('n', '<leader>tD', gitsigns.preview_hunk_inline, { desc = '[T]oggle git show [D]eleted' })
       end,
     },
