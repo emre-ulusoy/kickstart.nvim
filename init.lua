c89b37f4a11638c6fb9e626ec651650d5e4714d5
Emre Ulusoy <emre5tornaci@gmail.com>
2026-03-10 19:20:13 -0500

    added python to the lsp list, package updates

--- init.lua
+++ init.lua
@@ -161,7 +161,7 @@ vim.o.splitbelow = true
 --   See `:help lua-options`
 --   and `:help lua-guide-options`
 vim.o.list = true
-vim.opt.listchars = { tab = '▏ ', trail = '·', nbsp = '␣' } -- Some extra symbols: › » ↲ • · ➥ ▏ 
+vim.opt.listchars = { tab = '▏ ', trail = '·', nbsp = '␣' } -- Some extra symbols: › » ↲ • · ➥ ▏
 vim.opt.showbreak = '➥ '
 
 -- Preview substitutions live, as you type!
@@ -671,7 +671,7 @@ require('lazy').setup({
             },
           },
         },
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
